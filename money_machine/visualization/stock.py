441212a0f4bf48d1208b28d56d6eab53f4e1bf22
Adam Narozniak <narozniak.adam4@gmail.com>
2021-12-27 23:04:08 +0100

    Add stock prediction comparison visualization

---
+++ money_machine/visualization/stock.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+
+def plot_stock(y_test, y_pred, dates, n_ahead, n_days=None):
+    plt.figure(figsize=(16, 9))
+    plt.title(f"Stock Prediction for {n_ahead} ahead days", size=22)
+    plt.ylabel("Stock Close Price", size=16)
+    plt.xlabel("Date", size=16)
+    plt.plot(dates, y_test[:n_days], c="blue", label="ground truth")
+    plt.plot(dates, y_pred[:n_days], c="orange", label="predicted")
+    plt.legend(fontsize=14)
+    plt.show()
