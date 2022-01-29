5c8e331a0945cf14fc2ae86dd67d66dfb7d9a5ba
Adam Narozniak <narozniak.adam4@gmail.com>
2022-01-29 10:23:39 +0100

    Add pytrends and GTAB entities with common interface
    Both of them use Fetcher interface/abstract class

---
+++ money_machine/data/gtab_fetcher.py
@@ -0,0 +1,8 @@
+from money_machine.data.feacher import Fetcher
+
+
+class GtabFetcher(Fetcher):
+    def fetch_data(self, kw_list, timeframe):
+        self.requester.set_options(pytrends_config={"timeframe": timeframe})
+        self.requester.create_anchorbank()
+        return self.requester.new_query(kw_list[0])
---
+++ money_machine/data/pytrends_fetcher.py
@@ -0,0 +1,7 @@
+from money_machine.data.feacher import Fetcher
+
+
+class PytrendsFetcher(Fetcher):
+    def fetch_data(self, kw_list, timeframe):
+        self.requester.build_payload(kw_list, timeframe=timeframe)
+        return self.requester.interest_over_time()
