ab2c22bf0cf92c2644670f4c984ee73445e706f3
Adam Narozniak <narozniak.adam4@gmail.com>
2022-02-06 17:35:04 +0100

    Add a script for anchor banks creation

---
+++ scripts/create_anchor_banks.py
@@ -0,0 +1,18 @@
+import datetime as dt
+import pathlib
+
+import gtab
+
+from money_machine.data.gt_data import create_pulling_periods, create_anchor_banks
+from money_machine.data.gtab_fetcher import GtabFetcher
+
+if __name__ == "__main__":
+    end_date = dt.date(2022, 2, 1)
+    start_date = end_date - dt.timedelta(5 * 12 * 30 - 1)
+    period = dt.timedelta(30 * 9 - 1)  # this is the maximum daily period
+    overlap = dt.timedelta(100)
+    start_dates, end_dates = create_pulling_periods(start_date, end_date, overlap, period)
+    setup_path = pathlib.Path("./data/gtab/test_20220204")
+    t = gtab.GTAB(str(setup_path))
+    gtab_fetcher = GtabFetcher(t, str(setup_path))
+    create_anchor_banks(gtab_fetcher, start_dates, end_dates)
